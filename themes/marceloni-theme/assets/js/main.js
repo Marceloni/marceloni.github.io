281c2c7afa3124cb64c00c4089c93fdfd2225b8d
MarceloniGithub <marcelf8@outlook.com>
2024-01-17 16:41:54 +0100

    theme localstorage

--- themes/marceloni-theme/assets/js/main.js
+++ themes/marceloni-theme/assets/js/main.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    document.getElementById("theme-switch").getElementsByTagName("input")[0].checked = false
-    addEventListener("change", changeTheme)
+    setTimeout(()=>{
+        if(localStorage.getItem("theme")!="dark"){
+            document.body.classList.remove("dark")
+            document.getElementById("theme-switch").getElementsByTagName("input")[0].checked = true
+        }else {
+            document.getElementById("theme-switch").getElementsByTagName("input")[0].checked = false
+        }
+        
+        addEventListener("change", changeTheme)
+    },1)
 })
 function changeTheme() {
+    if(document.getElementById("theme-switch").getElementsByTagName("input")[0].checked){
+        document.body.classList.remove("dark")
+        localStorage.setItem("theme", "light")
+    }else {
+        document.body.classList.add("dark")
+        localStorage.setItem("theme", "dark")
+    }
     document.body.classList.toggle("dark")
 }
